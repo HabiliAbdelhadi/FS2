e4316c43703780ab25d25060320cd1499d126d66
HabiliAbdelhadi <habili.abdelhadi.98@gmail.com>
2023-12-20 14:50:11 +0100

    .dfg
    lkdmfg

--- client/src/components/WelcomeSection.jsx
+++ client/src/components/WelcomeSection.jsx
@@ -5,7 +5,7 @@ const WelcomeSection = () => {
     <Paper
       sx={{
         borderRadius: "0 12px 12px 0 ",
-        background: "#acf460",
+        background: "#1f3d2b",
         width: { xs: "97%", lg: "105%" },
       }}
     >
@@ -27,26 +27,30 @@ const WelcomeSection = () => {
             mt={3}
             // ml={24}
             gutterBottom
-            sx={{ color: "#505e11" }}
+            sx={{ color: "#74d143" }}
           >
             WELCOME TO MY PAGE
           </Typography>
           <Typography
             // ml={24}
+            color="white"
             typography={{ xs: "h5", sm: "h4" }}
           >
             My name is Billel Chelbi.
           </Typography>
           <Typography
             // mr={24}
+            color="white"
             typography={{ xs: "h5", sm: "h4" }}
             gutterBottom
           >
-            I’m a certified English teacher.
+            I’m a <span style={{ color: "#74d143" }}>certified</span> English
+            teacher.
           </Typography>
 
           <Typography
             //ml={24}
+            color="white"
             mr={2}
             typography="body1"
           >
@@ -61,15 +65,20 @@ const WelcomeSection = () => {
           item
           xs={12}
           sm={6}
-          mb={1}
           sx={{
             display: "flex",
             flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
+            justifyContent: { xs: "center", sm: "right" },
+            alignItems: { xs: "center", sm: "right" },
           }}
         >
-          <img src="picWelcome.png" alt="WelcomePic" width="75%"></img>
+          <img
+            height="100%"
+            src="welcomSec.png"
+            alt="WelcomePic"
+            width="75%"
+            style={{ borderRadius: "0 12px 12px 0 " }}
+          ></img>
         </Grid>
       </Grid>
     </Paper>
