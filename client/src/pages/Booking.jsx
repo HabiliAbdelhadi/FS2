d623f15c10663199e2c32954c915d9cea4c59a7e
HabiliAbdelhadi <habili.abdelhadi.98@gmail.com>
2024-01-01 11:00:18 +0100

    CalCom

--- client/src/pages/Booking.jsx
+++ client/src/pages/Booking.jsx
@@ -1,13 +1,31 @@
-import React from "react";
-import { ReactTidyCal } from "react-tidycal";
+/* First make sure that you have installed the package */
 
-import Container from "@mui/material/Container";
-const Booking = () => {
+/* If you are using yarn */
+// yarn add @calcom/embed-react
+
+/* If you are using npm */
+// npm install @calcom/embed-react
+
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+export default function Booking() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi();
+      cal.ns["30min"]("ui", {
+        theme: "dark",
+        styles: { branding: { brandColor: "#74d143" } },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
+  }, []);
   return (
-    <Container maxWidth="lg">
-      <ReactTidyCal path="habiliabdelhadi98/30-minute-meeting" />
-    </Container>
+    <Cal
+      namespace="30min"
+      calLink="abdelhadi-habili-s2fh6k/30min"
+      style={{ width: "100%", height: "100%", overflow: "scroll" }}
+      config={{ layout: "month_view" }}
+    />
   );
-};
-
-export default Booking;
+}
